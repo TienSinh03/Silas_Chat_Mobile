a55ed62cdc626a2fb12b43724068b4f2414614ae
TienSinh03 <zatos1232@gmail.com>
2025-03-17 23:33:25 +0700

    create home screen

--- Chat_Mobile/App.js
+++ Chat_Mobile/App.js
@@ -4,11 +4,14 @@ import {NavigationContainer} from '@react-navigation/native'
 import {createNativeStackNavigator} from '@react-navigation/native-stack'
 
 import Login from './src/pages/Login';
+import HomeScreen from './src/pages/HomeScreen';
+
 const Stack = createNativeStackNavigator()
 export default function App() {
   return (
     <NavigationContainer>
-        <Stack.Navigator initialRouteName='Login'>
+        <Stack.Navigator initialRouteName="HomeScreen">
+          <Stack.Screen name="HomeScreen" component={HomeScreen} options={{headerShown:false}}/>
           <Stack.Screen name="Login" component={Login} options={{headerShown:false}}/>
         </Stack.Navigator>
     </NavigationContainer>
