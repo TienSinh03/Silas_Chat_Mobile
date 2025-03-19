470e6dfc4d0203e6a57245f184a4b93bb059ff32
vantoan24-iuh <vantoan24-iuh>
2025-03-19 22:08:52 +0700

    update

---
+++ Chat_Mobile/src/components/PhoneBookGroup.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const PhoneBookGroup = () => {
+  return (
+    <View>
+      <Text>Danh bạ nhóm</Text>
+    </View>
+  );
+};
+
+export default PhoneBookGroup;
--- Chat_Mobile/src/navigation/TabBottom.js
+++ Chat_Mobile/src/navigation/TabBottom.js
@@ -19,7 +19,7 @@ import ConversationScreen from "../pages/ConversationScreen";
 import SingleChatScreen from "../pages/SingleChatScreen";
 import DetailSingleChatScreen from "../pages/DetailSingleChatScreen";
 import Login from "../pages/Login";
-
+import Phonebook  from "../pages/Phonebook";
 const { width, height } = Dimensions.get("window");
 
 const Tab = createBottomTabNavigator();
@@ -30,83 +30,58 @@ const TabBottom = () => {
                 screenOptions={({ route }) => ({
                     tabBarIcon: ({ focused, color, size }) => {
                         let iconName;
-
-                        if (route.name === "Tin nhắn") {
-                            iconName = "wechat";
-                            return (
-                                <IconAnt
-                                    name={iconName}
-                                    size={size}
-                                    color={color}
-                                />
-                            );
-                        } else if (route.name === "Danh bạ") {
-                            iconName = "contacts";
-                            return (
-                                <IconAnt
-                                    name={iconName}
-                                    size={size}
-                                    color={color}
-                                />
-                            );
-                        } else if (route.name === "Nhật ký") {
-                            iconName = "clockcircleo";
-                            return (
-                                <IconAnt
-                                    name={iconName}
-                                    size={size}
-                                    color={color}
-                                />
-                            );
-                        } else if (route.name === "Cá nhân") {
-                            iconName = "user";
-                            return (
-                                <IconAnt
-                                    name={iconName}
-                                    size={size}
-                                    color={color}
-                                />
-                            );
-                        }
-                    },
-                    tabBarActiveTintColor: "#006AF5",
-                    tabBarInactiveTintColor: "gray",
-                    tabBarStyle: {
-                        borderTopWidth: 0,
-                        elevation: 0,
-                        height: 65,
-                        paddingBottom: 10,
-                    },
-                    tabBarLabelStyle: {
-                        fontSize: 16,
-                        fontWeight: "bold",
-                        marginBottom: 10,
-                    },
-                })}
-            >
-                <Tab.Screen
-                    name="Tin nhắn"
-                    component={ConversationScreen}
-                    options={{ headerShown: false }}
-                />
-                <Tab.Screen
-                    name="Danh bạ" // Đổi tên thành Dictionary (hoặc tên khác)
-                    component={ConversationScreen} // Nhớ đổi tên lại
-                    options={{ headerShown: false }}
-                />
-                <Tab.Screen
-                    name="Nhật ký" // Đổi tên thành Dictionary (hoặc tên khác)
-                    component={ConversationScreen} // Nhớ đổi tên lại
-                    options={{ headerShown: false }}
-                />
-                <Tab.Screen
-                    name="Cá nhân" // Đổi tên thành Dictionary (hoặc tên khác)
-                    component={ConversationScreen} // Nhớ đổi tên lại
-                    options={{ headerShown: false }}
-                />
-            </Tab.Navigator>
-        </>
-    );
+            if (route.name === "Tin nhắn") {
+              iconName = "wechat";
+              return <IconAnt name={iconName} size={size} color={color} />;
+            } else if (route.name === "Danh bạ") {
+              iconName = "contacts";
+              return <IconAnt name={iconName} size={size} color={color} />;
+            } else if (route.name === "Nhật ký") {
+              iconName = "clockcircleo";
+              return <IconAnt name={iconName} size={size} color={color} />;
+            } else if (route.name === "Cá nhân") {
+              iconName = "user";
+              return <IconAnt name={iconName} size={size} color={color} />;
+            }
+          },
+          tabBarActiveTintColor: "#006AF5",
+          tabBarInactiveTintColor: "gray",
+          tabBarStyle: {
+            borderTopWidth: 0,
+            elevation: 0,
+            height: 65,
+            paddingBottom: 10,
+          },
+          tabBarLabelStyle: {
+            fontSize: 16,
+            fontWeight: "bold",
+            marginBottom: 10,
+          },
+        })}
+      >
+        <Tab.Screen
+          name="Tin nhắn"
+          component={ConversationScreen}
+          options={{ headerShown: false }}
+        />
+        <Tab.Screen
+          name="Danh bạ" // Đổi tên thành Dictionary (hoặc tên khác)
+          component={Phonebook} // Nhớ đổi tên lại
+          options={{ headerShown: false }}
+        />
+        <Tab.Screen
+          name="Nhật ký" // Đổi tên thành Dictionary (hoặc tên khác)
+          component={ConversationScreen} // Nhớ đổi tên lại
+          options={{ headerShown: false }}
+        />
+        <Tab.Screen
+          name="Cá nhân" // Đổi tên thành Dictionary (hoặc tên khác)
+          component={ConversationScreen} // Nhớ đổi tên lại
+          options={{ headerShown: false }}
+        />
+      </Tab.Navigator>
+    </>
+  )                    
 };
 
 export default TabBottom;
